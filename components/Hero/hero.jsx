445f25d74c0be6a030d56faa4dd5f52290f7bf0c
andrea2601 <andrealogiudice26@gmail.com>
2022-05-30 17:42:56 +0200

    feat: hero

--- components/Hero/hero.jsx
+++ components/Hero/hero.jsx
@@ -1,9 +1,15 @@
 import styles from "./styles.module.scss";
-
+import Image from 'next/dist/client/image'
+import heroImg from "../Images/heroImg.jpg"
 export const Hero = () => {
   return (
     <div className={styles.Hero}>
-      <h1>Ciao</h1>
+      <div className={styles.img}>
+        <Image src={heroImg} alt="heroImg" layout="fill"/>
+      </div>
+      <h1 className={styles.title}>Vivi viaggi unici al mondo...</h1> 
+      <h2 className={styles.subtitle}>...e incontra nuovi amici</h2> 
+      <button className={styles.btn}>Scopri tutti i viaggi</button>
     </div>
   );
 };
