9effd08bcbaed704140e4b1fe49c9b8bca8e2f30
ImBlue444 <dennis.badagliacca444@gmail.com>
2022-05-31 16:40:51 +0200

    Feat: Navbar

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -3,9 +3,11 @@ import style from "../Navbar/style.module.scss"
 import logo from "../Images/logo.png"
 import Image from 'next/dist/client/image'
 import { useState } from 'react'
+import SideMenu from '../SideMenu'
 
 
 export default function Navbar() {
+    
     const [isActive,setIsActive]=useState(true);
 
     const ToggleMenu = () =>{
@@ -13,8 +15,8 @@ export default function Navbar() {
     }
     
   return (
-      
     <div className={style.Navbar}>
+    <div className={style.NavbarElements}>
         <nav>
             <ul>
                 <li>
@@ -24,14 +26,21 @@ export default function Navbar() {
                     </div>
                     </Link>
                 </li>
+                <div className={style.burgerMenu}>
+                <li className={style.container}>
 
-                <li className={style.RightMenu}>
-                <div className={isActive? "fa-solid fa-bars":"fa-solid fa-xmark"} onClick={ToggleMenu}></div>
+                <div className={isActive? style.container: style.change} onClick={ToggleMenu}>
+                <div className={style.bar1}></div>
+                <div className={style.bar2}></div>
+                <div className={style.bar3}></div>
+                </div>
 
                 </li>
-
+                </div>
             </ul>
         </nav>
     </div>
+            <SideMenu activator={isActive ? false: true} />
+            </div>
   )
 }
