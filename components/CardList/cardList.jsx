7a50ba3cf2888871495ba3ae6ec5f30dff62c1a0
Chiara <chiara-zuc@libero.it>
2022-05-31 17:42:18 +0200

    fix: cardlist

--- components/CardList/cardList.jsx
+++ components/CardList/cardList.jsx
@@ -1,19 +1,11 @@
-import { useEffect, useState } from "react";
-import { get } from "../Utils/index";
 import { MediumCard } from "../Cards/MediumCard/mediumCard";
-import { SmallCard } from "../Cards/SmallCard/smallCard";
-import { LargeCard } from "../Cards/LargeCard/LargeCard";
 
 export const CardList = () => {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    get().then((data) => setData(data));
-    console.log("---->", data);
-  }, []);
-
   return (
     <>
+      <MediumCard item={data && data.city.name} />
+      <MediumCard item={data && data.city.name} />
+      <MediumCard item={data && data.city.name} />
       <MediumCard item={data && data.city.name} />
     </>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
-
 import { DefaultLayout } from "../components/DefaultLayout/defaultlayout";
-import '../styles/globals.css'
+import "../styles/globals.css";
 import Provider from "../Context/Provider";
 function MyApp({ Component, pageProps }) {
-  return(
-  <Provider> 
-    <DefaultLayout>
-    <Component {...pageProps} /> 
-    </DefaultLayout>
-  </Provider>)
-
+  return (
+    <Provider>
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
+    </Provider>
+  );
 }
 
 export default MyApp;
