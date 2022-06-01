c53944868371391106a45a34c416434dadca6428
chry902 <tchry902@gmail.com>
2022-06-01 12:37:15 +0200

    completed responsive large card

--- components/CardListLarge/cardListLarge.jsx
+++ components/CardListLarge/cardListLarge.jsx
@@ -1,9 +1,9 @@
-
+import styles from "./styles.module.scss";
 import { useEffect } from "react";
-import { useVacationContext } from "../../Context/Provider"
+import { useVacationContext } from "../../Context/Provider";
 import { LargeCard } from "../Cards/LargeCard/largeCard";
 
-export const CardListLarge = ({country}) => {
+export const CardListLarge = ({ country }) => {
   const {
     fetchAll,
     state: { vacation },
@@ -15,10 +15,13 @@ export const CardListLarge = ({country}) => {
   }, []);
 
   return (
-    <>
-      {
-        vacation && vacation.filter(item => item.country.name.toLowerCase() === country.toLowerCase()).map(city => <LargeCard city={city} />)
-      }
-    </>
+    <div className={styles.cardLargeContiner}>
+      {vacation &&
+        vacation
+          .filter(
+            (item) => item.country.name.toLowerCase() === country?.toLowerCase()
+          )
+          .map((city) => <LargeCard key={city.id} city={city} />)}
+    </div>
   );
 };
