9eef59f57718cc0c4f571860bf9651ba3c42c328
Chiara <chiara-zuc@libero.it>
2022-06-01 18:48:27 +0200

    feat: added price section

--- pages/country/city/[id].jsx
+++ pages/country/city/[id].jsx
@@ -1,19 +1,20 @@
-
 import { useRouter } from "next/router";
 import styles from "./[id].module.scss";
+import { Details } from "../../../components/Details/details";
 // import {CardListLarge} from '../../components/CardListLarge/cardListLarge';
-import {Cart } from "../../../components/Cart/cart"
+import { Cart } from "../../../components/Cart/cart";
+import { Price } from "../../../components/Price/price";
 
 const DynamicCity = () => {
   const router = useRouter();
   const { id } = router.query;
-console.log("city: ",id);
+  console.log("city: ", id);
   return (
     <div className={styles.DynamicCountry}>
-    <h1>
-      {/* {id} */}
-    </h1>
+      <h1>{/* {id} */}</h1>
       {/* <CardListLarge country={id}/> */}
+      <Price />
+      <Details />
     </div>
   );
 };
