8eb7ed0f011bc1ec6475eccb7d677a9434230105
chry902 <tchry902@gmail.com>
2022-06-04 19:35:25 +0200

    feat:edit font size inpunt

--- pages/country/[id].jsx
+++ pages/country/[id].jsx
@@ -1,23 +1,22 @@
 import { useRouter } from "next/router";
 import styles from "./[id].module.scss";
 
-
-import {CardListLarge} from '../../components/CardListLarge/cardListLarge';
-import  {Hero}  from "../../components/Hero/hero";
+import { CardListLarge } from "../../components/CardListLarge/cardListLarge";
+import { Hero } from "../../components/Hero/hero";
 
 import { useVacationContext } from "../../Context/Provider";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const DynamicCountry = () => {
-  const {
-    fetchAll,
-    state: { vacation, italia, spagna, francia, romania },
-  } = useVacationContext();
+  // const {
+  //   fetchAll,
+  //   state: { vacation, italia, spagna, francia, romania },
+  // } = useVacationContext();
 
-  useEffect(() => {
-    fetchAll("/cities");
-  }, []);
-  
+  // useEffect(() => {
+  //   fetchAll("/cities");
+  // }, []);
+
   const router = useRouter();
   const { id } = router.query;
 
@@ -26,28 +25,10 @@ const DynamicCountry = () => {
   return (
     <div className={styles.DynamicCountry}>
       <Hero title={title} subtitle={id} />
-      <h1>
-        {id}
-      </h1>
-
-
-//   const [heroImg, setHeroImg] = useState("");
-
-//   useEffect(() => {
-//     const imgs = vacation?.filter(
-//       (item) => item.country.name.toLowerCase() === id?.toLowerCase()
-//     );
-//     const i = Math.floor(Math.random() * imgs.length);
-//     console.log("imgs: ", imgs);
-//     setHeroImg(imgs[i]?.cover_image_url);
-//   }, [vacation]);
-
-//   return (
-//     <div className={styles.DynamicCountry}>
-//       <Hero title={title} subtitle={id} img={heroImg} />
-
-
-      <CardListLarge country={id} />
+      <h1>{id}</h1>
+      <div className={styles.DynamicCountry}>
+        <CardListLarge country={id} />
+      </div>
     </div>
   );
 };
