b663059a3e26ab1dff1f85573784c1e95f8c3d40
chry902 <tchry902@gmail.com>
2022-06-04 17:32:09 +0200

    prova

--- components/CardList/cardList.jsx
+++ components/CardList/cardList.jsx
@@ -1,18 +1,27 @@
 import styles from "./styles.module.scss";
 import { MediumCard } from "../Cards/MediumCard/mediumCard";
+import { useVacationContext } from "../../Context/Provider";
 
 export const CardList = () => {
+  const {
+    state: { italiaImg, spagnaImg, franciaImg, romaniaImg },
+  } = useVacationContext();
+
   const itDescript = "Esplora le meraviglie d'italia";
-  const img = "italia";
+
   const espDescript = "Esplora las meravillas de España";
   const fraDescript = "Explorez les merveilles de la France";
   const englDescript = "Explore the wonders of Romania";
   return (
     <div className={styles.cardContiner}>
-      <MediumCard name={"Italia"} description={itDescript} img={img} />
-      <MediumCard name={"Spagna"} description={espDescript} />
-      <MediumCard name={"Francia"} description={fraDescript} />
-      <MediumCard name={"Romania"} description={englDescript} />
+      <MediumCard name={"Italia"} description={itDescript} img={italiaImg} />
+      <MediumCard name={"Spagna"} description={espDescript} img={spagnaImg} />
+      <MediumCard name={"Francia"} description={fraDescript} img={franciaImg} />
+      <MediumCard
+        name={"Romania"}
+        description={englDescript}
+        img={romaniaImg}
+      />
     </div>
   );
 };
