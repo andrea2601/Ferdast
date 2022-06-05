98cd9086ef5cd302330bf994dbc600def8ee1d78
chry902 <tchry902@gmail.com>
2022-06-05 15:21:01 +0200

    feat:edit map

--- components/Cards/MediumCard/mediumCard.jsx
+++ components/Cards/MediumCard/mediumCard.jsx
@@ -1,16 +1,8 @@
 import styles from "./styles.module.scss";
 
 import Link from "next/link";
-import { useState } from "react";
 
 export const MediumCard = (props) => {
-  // const [img, setImg] = useState();
-  // if (props.img == "italia") {
-  //   setImg(styles.italia);
-  // } else if (props.img == "francia") {
-  //   setImg(styles.francia);
-  // }
-  // const img = "styles.italia";
   return (
     <div className={`${styles.mediumCardContainer} `}>
       <div className={styles.imgContainer}>
