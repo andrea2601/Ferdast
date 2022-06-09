be2a3bb80004dc88f8be4500d341d2536db752fb
ImBlue444 <dennis.badagliacca444@gmail.com>
2022-06-09 12:49:40 +0200

    Fix: Service Worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import { DefaultLayout } from "../components/DefaultLayout/defaultlayout";
 import "../styles/globals.css";
 import Provider from "../Context/Provider";
+import { useEffect } from "react";
+useEffect;
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <Provider>
       <DefaultLayout>
