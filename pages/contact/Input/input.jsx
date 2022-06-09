d17a7047f85c9ef213753d1ef4fd0b858694cf0d
Chiara <chiara-zuc@libero.it>
2022-06-09 11:49:39 +0200

    fix: contact page

--- pages/contact/Input/input.jsx
+++ pages/contact/Input/input.jsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
-export const Input = () => {
+export const Input = ({ text, id, setValue, value }) => {
+  const [clicked, setClicked] = useState(false);
+  const [empty, setEmpty] = useState("");
+
   return (
     <div className={styles.inputGroup}>
-      <label htmlFor="fname">First name</label>
-      <input
-        className={styles.input}
-        type="text"
-        id="fname"
-        name="firstname"
-        // placeholder="Your name.."
-        // required
-      />
+      <label
+        htmlFor={id}
+        className={clicked ? styles.labelOn : styles.labelOff}
+        onClick={() => setClicked(!clicked)}
+      >
+        {text}
+      </label>
+      {text.includes("messaggio") ? (
+        <textarea
+          className={clicked ? styles.textarea : styles.hideInput}
+          id={id}
+          name={id}
+          value={empty}
+          onChange={(e) => setEmpty(e.target.value)}
+        ></textarea>
+      ) : (
+        <input
+          className={clicked ? styles.input : styles.hideInput}
+          type={
+            text.toLowerCase() == "nome" && text.toLowerCase() == "cognome"
+              ? "text"
+              : text.toLowerCase() == "email"
+              ? "email"
+              : ""
+          }
+          id={id}
+          name={text.toLowerCase()}
+          value={empty}
+          onChange={(e) => setEmpty(e.target.value)}
+          required
+        />
+      )}
     </div>
   );
 };
